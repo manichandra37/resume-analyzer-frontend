093d7d5378c859b26744e15b2f44a4d1f74c8d93
manichandra37 <manichandramaddi73@gmail.com>
2026-05-26 03:56:43 -0500

    added register and login section

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
-import { loginUser } from "../services/api";
+import { loginUser, registerUser } from "../services/api";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [name, setName] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
 
+  // <-- handleLogin -->
   const handleLogin = async () => {
     try {
       const response = await loginUser(email, password);
@@ -17,27 +21,85 @@ const LoginPage = () => {
         alert("Server not reachable");
       }
     }
-  };  // <-- handleLogin ends HERE
+  };
 
-  // return belongs to LoginPage, NOT inside handleLogin
+  const handleRegister = async () => {
+    try {
+      const response = await registerUser(name, email, password, phoneNumber);
+      console.log(response.data);
+      alert("Registration Successful! Please login.");
+      setIsLogin(true);
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("Server not reachable");
+      }
+    }
+  };
+
   return (
     <div>
-      <h1>Login</h1>
-      <input
-        type="text"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <br/><br/>
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <br/><br/>
-      <button onClick={handleLogin}>Login</button>
+      {isLogin ? (
+        <div>
+          <h1>Login</h1>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <br />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <br />
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      ) : (
+        <div>
+          <h1>Register</h1>
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br></br>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br></br>
+          <input
+            type="text"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br></br>
+          <input
+            type="text"
+            placeholder="Phone Number"
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+          <br></br>
+          <button onClick={handleRegister}>Register</button>
+        </div>
+      )}
+      <button onClick={() => setIsLogin(!isLogin)}>
+        {isLogin
+          ? "Don't have an account? Register"
+          : "Already have an account? Login"}
+      </button>
     </div>
   );
 };
