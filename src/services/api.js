da5d9949a0985b8db483b931ea29953cb616b191
manichandra37 <manichandramaddi73@gmail.com>
2026-06-03 19:32:10 -0500

    update API URL to production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://resume-analyzer-production-ace9.up.railway.app/api",
 });
 
 export const loginUser = (email, password) => {
